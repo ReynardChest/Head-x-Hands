4277c8fb73a2cbc0b925b546f46b50f8434455e2
ReynardChest <48452807+ReynardChest@users.noreply.github.com>
2023-09-28 15:11:14 +0300

    Add files via upload

---
+++ QA_AUTOMATION_test.py
@@ -0,0 +1,37 @@
+import random as rnd
+
+    
+def sort_array_by_parity(array, array_num):
+    sorted_array = array
+        
+    if array_num%2 == 0:
+        sorted_array.sort(reverse = True)       
+    elif array_num%2 != 0:
+        sorted_array.sort(reverse = False)
+        
+    return sorted_array
+        
+def form_array(array_length):
+    min_val = -10000
+    max_val = 10000
+    formed_array =  [rnd.randint(min_val, max_val) for _ in range(array_length)]
+        
+    return formed_array
+    
+def form_massive2(num_of_iter):
+    unused_len =list(range(0, num_of_iter*10))
+    used_len = rnd.sample(unused_len, num_of_iter)
+    formed_massive = []
+    array_num = 0
+        
+    for i in used_len:            
+        cur_array = form_array(i)
+        sort_array_by_parity(cur_array,array_num)
+        array_num += 1
+        formed_massive.append(cur_array)
+        
+    return formed_massive
+
+if __name__ == '__main__':
+    new_massive = form_massive2(100)
+
